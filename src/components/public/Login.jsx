fc5021137e732f172b700a442b8f4901910b1d65
Joseph <josephgreat2003@gmail.com>
2023-08-14 16:47:25 +0100

    added google auth

--- src/components/public/Login.jsx
+++ src/components/public/Login.jsx
@@ -1,129 +1,134 @@
-import { useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import AuthIcons from '../common/AuthIcons'
-import TermsOfService from '../common/TermsOfService'
-import Banner from '../common/Banner'
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import AuthIcons from "../common/AuthIcons";
+import TermsOfService from "../common/TermsOfService";
+import Banner from "../common/Banner";
 //import { useLoginUserMutation } from '../../services/authApi'
-import { loginUser } from '../../apis/authenticationApis'
+import { loginUser } from "../../apis/authenticationApis";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const Login = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  /**const [email, setEmail] = useState();*/
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [signing, setSigning] = useState(false);
+  const [success, setSuccess] = useState(null);
+  const [error, setError] = useState(null);
 
-    /**const [email, setEmail] = useState();*/
-    const [username, setUsername] = useState();
-    const [password, setPassword] = useState();
-    const [signing, setSigning] = useState(false);
-    const [success, setSuccess] = useState(null);
-    const [error, setError] = useState(null);
+  //const [loginUser, {data, isError, error}] = useLoginUserMutation();
 
-    //const [loginUser, {data, isError, error}] = useLoginUserMutation();
+  useEffect(() => {
+    if (success !== null) {
+      localStorage.setItem(
+        "login",
+        JSON.stringify({
+          key: success.key,
+        })
+      );
 
-    useEffect(() => {
-        if(success !== null) {
-            localStorage.setItem("login", JSON.stringify({
-                key: success.key
-            }));
-
-            setError("");
-            setUsername("");
-            setPassword("");
-            navigate('/home');
-            window.location.reload();
-        }
-
-        if(error !== null){
-            alert(error.non_field_errors[0]);
-            setSigning(false);
-            setError(null);
-        }
-
-    }, [success, error]);
-
-    const handleLogin = (e) => {
-
-        e.preventDefault();
-
-        const data = {
-            username, password
-        }
-        
-        loginUser(data, setSuccess, setError, setSigning);
+      setError("");
+      setUsername("");
+      setPassword("");
+      navigate("/home");
+      window.location.reload();
     }
 
+    if (error !== null) {
+      alert(error.non_field_errors[0]);
+      setSigning(false);
+      setError(null);
+    }
+  }, [success, error]);
 
-    useEffect(() => {
-        if(localStorage.getItem('login') !== null){
-            navigate('/dashboard');
-        }
-    }, [navigate])
+  const handleLogin = (e) => {
+    e.preventDefault();
 
+    const data = {
+      username,
+      password,
+    };
 
-    return (
-        <div className='grid grid-cols-1 md:grid-cols-2 px-6 lg:px-24 mt-4'>
+    loginUser(data, setSuccess, setError, setSigning);
+  };
 
-            <Banner />
+  useEffect(() => {
+    if (localStorage.getItem("login") !== null) {
+      navigate("/dashboard");
+    }
+  }, [navigate]);
 
-            <div className='md:px-6 lg:px-16 md:mt-12'>
-                <div className='flex justify-start text-gray-300'>
-                    <div className='text-lg py-2 border-b-2 border-blue-500'>Sign in</div>
-                    <div className='grow border-b-2 border-gray-300'></div>
-                </div>
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 px-6 lg:px-24 mt-4">
+      <Banner />
 
-                <div className='my-12'>
-                    <form onSubmit={handleLogin} className='space-y-4'>
-                        <div>
-                            <div className='relative z-10 mb-[-12px] ml-3 text-gray-300 text-md bg-black max-w-max'>Username</div>
-                            <input 
-                                type='text'
-                                className='w-full bg-transparent border border-gray-800 rounded-md p-3 text-gray-500 placeholder-gray-700'
-                                placeholder='Username'
-                                onChange={(e) => setUsername(e.target.value)}
-                                required
-                            />
-                        </div>
-
-                        <div>
-                            <div className='relative z-10 mb-[-12px] ml-3 text-gray-300 text-md bg-black max-w-max'>Password</div>
-                            <input 
-                                type='password'
-                                className='w-full bg-transparent border border-gray-800 rounded-md p-3 text-gray-500 placeholder-gray-700'
-                                placeholder='Enter password'
-                                onChange={(e) => setPassword(e.target.value)}
-                                required
-                            />
-                        </div>
-
-                        <div>
-                            <button 
-                                className='w-full p-3 bg-[#007aff] hover:bg-[#0d4580] rounded-lg text-white mt-6 mb-2'
-                            >
-                                {signing ? 'Logging in...' : 'Login'}
-                            </button>
-                            
-                            <div className='w-full flex justify-end text-md text-gray-300'>
-                                <Link to='/login'>Forgot Password?</Link>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-
-                <div className='flex justify-between text-gray-300 space-x-4 items-center'>
-                    <div className='grow border border-gray-500 h-0'></div>
-                    <div>Or signin with</div>
-                    <div className='grow border border-gray-500 h-0'></div>
-                </div>
-
-                <AuthIcons />
-
-                <div className='flex justify-center text-gray-400 space-x-1 my-10'>
-                    <span>{"Don't have an Account?"}</span> <Link to='/register' className='text-[#007aff]'>Sign up</Link>
-                </div>
-
-                <TermsOfService />
-            </div>
+      <div className="md:px-6 lg:px-16 md:mt-12">
+        <div className="flex justify-start text-gray-300">
+          <div className="text-lg py-2 border-b-2 border-blue-500">Sign in</div>
+          <div className="grow border-b-2 border-gray-300"></div>
         </div>
-    )
-}
 
-export default Login
+        <div className="my-12">
+          <form onSubmit={handleLogin} className="space-y-4">
+            <div>
+              <div className="relative z-10 mb-[-12px] ml-3 text-gray-300 text-md bg-black max-w-max">
+                Username
+              </div>
+              <input
+                type="text"
+                className="w-full bg-transparent border border-gray-800 rounded-md p-3 text-gray-500 placeholder-gray-700"
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+
+            <div>
+              <div className="relative z-10 mb-[-12px] ml-3 text-gray-300 text-md bg-black max-w-max">
+                Password
+              </div>
+              <input
+                type="password"
+                className="w-full bg-transparent border border-gray-800 rounded-md p-3 text-gray-500 placeholder-gray-700"
+                placeholder="Enter password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+
+            <div>
+              <button className="w-full p-3 bg-[#007aff] hover:bg-[#0d4580] rounded-lg text-white mt-6 mb-2">
+                {signing ? "Logging in..." : "Login"}
+              </button>
+
+              <div className="w-full flex justify-end text-md text-gray-300">
+                <Link to="/login">Forgot Password?</Link>
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <div className="flex justify-between text-gray-300 space-x-4 items-center">
+          <div className="grow border border-gray-500 h-0"></div>
+          <div>Or signin with</div>
+          <div className="grow border border-gray-500 h-0"></div>
+        </div>
+        <GoogleOAuthProvider clientId="524267745289-99tcul9q2eos9crnc5krameenh2p59gb.apps.googleusercontent.com">
+          <AuthIcons />
+        </GoogleOAuthProvider>
+
+        <div className="flex justify-center text-gray-400 space-x-1 my-10">
+          <span>{"Don't have an Account?"}</span>{" "}
+          <Link to="/register" className="text-[#007aff]">
+            Sign up
+          </Link>
+        </div>
+
+        <TermsOfService />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
