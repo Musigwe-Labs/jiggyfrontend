294fbbc651a60368f9d683feb2c8e313fb6deec0
chinedu.ijeomah <chinedu.ijeomah@yahoo.com>
2023-08-14 06:16:28 +0100

    updated navigation bar background and highlighted current page icon

--- src/components/public/home/homeFooter.jsx
+++ src/components/public/home/homeFooter.jsx
@@ -3,11 +3,15 @@ import { AiFillHome, AiOutlineLogout, AiOutlineUser } from "react-icons/ai";
 import { RxEnvelopeClosed } from "react-icons/rx";
 import { HiOutlineBell } from "react-icons/hi";
 import { MdOutlineDashboard } from "react-icons/md"
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 const HomeFooter = ()=>{
 
     const navigate = useNavigate();
+
+    const location = useLocation();
+
+    const urlpath = location.pathname;
 
     const logout = () => {
         localStorage.removeItem('login');
@@ -15,23 +19,51 @@ const HomeFooter = ()=>{
         window.location.reload();
     }
 
+
+    const navlinks = [
+        {
+            id: 1,
+            url: "/dashboard",
+            icon: <MdOutlineDashboard size={20} />,
+        },
+        {
+            id: 2,
+            url: "/home",
+            icon: <AiFillHome size={20} />,
+        },
+        {
+            id: 3,
+            url: "/messages",
+            icon: <RxEnvelopeClosed size={20} />,
+        },
+        {
+            id: 4,
+            url: "/profile",
+            icon: <AiOutlineUser size={20} />,
+        },
+        {
+            id: 5,
+            url: "/messages",
+            icon: <HiOutlineBell size={20} />,
+        },
+    ]
+
     return (
-        <div className='w-full flex justify-center fixed bottom-0 bg-[#0c0c0c] space-x-12 p-4'>
-            <Link to='/dashboard' className='cursor-pointer text-gray-400 hover:text-green-400'>
-                <MdOutlineDashboard size={20} />
-            </Link>
-            <Link to='/home' className='cursor-pointer text-gray-400 hover:text-green-400'>
-                <AiFillHome size={20} />
-            </Link>
-            <Link to='/messages' className='cursor-pointer text-gray-400 hover:text-green-400'>
-                <RxEnvelopeClosed size={20} />
-            </Link>
-            <Link to='/profile' className='cursor-pointer text-gray-400 hover:text-green-400'>
-                <AiOutlineUser size={20} />
-            </Link>
-            <span className='cursor-pointer text-gray-400 hover:text-green-400'>
-                <HiOutlineBell size={20} />
-            </span>
+        <div className='w-full flex justify-center fixed bottom-0 bg-gray-900 space-x-10 p-4'>
+            {
+                navlinks.map(nav => {
+                    return(
+                        <Link 
+                            to={nav.url} 
+                            key={nav.id} 
+                            className={`cursor-pointer ${urlpath === nav.url ? 'text-green-400' : 'text-gray-400'} hover:text-green-400`}
+                        >
+                            {nav.icon}
+                        </Link>
+                    )
+                })
+            }
+            
             <span onClick={() => logout()} className='cursor-pointer text-red-600 hover:text-red-800'>
                 <AiOutlineLogout size={20} />
             </span>
