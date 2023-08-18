aa53923819ff228bc645e93e21004f7982b85e43
Evan <egwomevan323@gmail.com>
2023-08-18 18:50:08 +0100

    changes on the profilepage

--- src/components/public/home/Home.jsx
+++ src/components/public/home/Home.jsx
@@ -34,7 +34,6 @@ const Home = () => {
       <CreatePostPage setCreatePost={setCreatePost}/>
     )
   }
-
   return (
     <div>
     {profilePage ? <Profile setProfilePage={setProfilePage}/> :''}
--- src/components/public/home/homeHeader.jsx
+++ src/components/public/home/homeHeader.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable react/prop-types */
 import profile_pic from '../../../assets/profile_pics/pic1.png'
+import { useContext } from 'react'
+import { AuthContext } from '../../../contexts/AuthContext'
 
 const HomeHeader = ({ setProfilePage }) => {
+  const { key } = useContext(AuthContext);
     return (
       <div className='flex items-center pt-6'>
-        <div onClick={()=>setProfilePage(true)}>
+        <div onClick={()=>{ 
+          (key === '')? window.location.href('/login') :setProfilePage(true)}}>
           <img className='w-14 rounded-full mx-[9px]' src={profile_pic} />
         </div>
         <h1 className='text-3xl text-center font-bold from-[#f33f5e] via-[#ff008a9e] to-[#b416fe66] bg-gradient-to-r bg-clip-text text-transparent'>Home</h1>
