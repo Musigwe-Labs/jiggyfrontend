8aa2fa22794cb891e0f85fa67b9155bc1c49de02
Evan <egwomevan323@gmail.com>
2023-08-20 10:46:30 +0100

    added some style on the comment

--- src/components/public/home/comments.jsx
+++ src/components/public/home/comments.jsx
@@ -3,6 +3,7 @@ import { useContext, useState , useEffect } from 'react'
 import { AuthContext } from '../../../contexts/AuthContext'
 import CommentInfo from './commentInfo'
 import Gist from './gist'
+import GistLinks from './gistLinks'
 import profile_pic from '../../../assets/profile_pics/pic1.png'
 import { userInfo } from '../../../apis/authenticationApis'
 import { FaArrowLeftLong } from 'react-icons/fa6'
@@ -36,36 +37,40 @@ const Comment =({post , setSelectedPost })=>{
     }
 
     return(
-        <div className='z-50 h-screen bg-[#321616] pt-6 px-3'>
-            <FaArrowLeftLong size={25} onClick={()=>setSelectedPost(null)}/>
-            <div className='z-10 mx-2 md:mx-16 bg-[#110101] p-3 rounded-3xl'>
-              <CommentInfo school={post.user.school} name={post.user.generated_username} />
-              <span className='text-sm text-gray-400 ml-8'>{'17h'}</span>
-              <Gist post={post} />
+        <div className='z-50 h-screen pt-4 px-3'>
+            <div className='flex align-center'>
+                <FaArrowLeftLong size={25} onClick={()=>setSelectedPost(null)}/>
+                <h1 className='text-3xl ml-6 text-center font-bold from-[#ff0000] via-[#ff004c] to-[#0028ad] bg-gradient-to-br bg-clip-text text-transparent'>Comment</h1>
             </div>
-                <p className='px-3 my-3 text-gray-400'>{post.comments.length} comments</p>
-            <div className='mx-2 max-h-[100%] overflow-auto'>
+            <div className='z-10 md:mx-10 p-3'>
+              <CommentInfo school={post.user.school} name={post.user.generated_username} />
+              <Gist post={post} />
+              <GistLinks post={post}/>
+            </div>
+            <p className='px-3 my-3 text-gray-400'>{post.comments.length} comments</p>
+            <div className='mx-2 max-h-[100%] overflow-auto border-l border-gray-500'>
                 {
                     post.comments.map((comment) =>{
                         return (
-                        <div key={comment.created_at} className='text-base mt-2 bg-[#110101] p-3 rounded-3xl'>
-                            <div className='flex items-center'>
+                        <div key={comment.created_at} className='text-base bg-[#1717171a] mt-2 px-3 rounded-xl'>
+                            <div className='flex items-center mb-1'>
                                 <img className='w-6 rounded-3xl mr-2' src={profile_pic} alt='profile-img' />
                                 <h4 className='text-white mr-1 text-base font-bold'>{comment.name}</h4>
                             </div>
-                            <p>{comment.content}</p>
+                            <p className='text-base'><span className='text-[14.5px] text-blue-500 mr-2 font-light'>@{post.user.generated_username}</span>{comment.content}</p>
                         </div>
                         )
                       })
                 }
             </div>
-            <div className='absolute w-[92%] grid place-items-end bottom-0 p-1 bg-[#321616]'>
+            <div className='absolute w-[100%] border-t border-gray-500 left-0 bottom-0 px-2 bg-black'>
                 <input 
-                type='text' 
-                className='h-[50%] mb-[10px] w-full rounded border-cyan-600 text-black p-2'
+                type='text'
+                placeholder='Comment your thought'
+                className='h-[50%] mb-[10px] w-[80%] border-b border-gray-600 bg-transparent text-black p-2'
                 value={content}
                 onChange={handleTextareaChange}/>
-                <button className='float-right bg-[#5357B6] py-1 px-2 rounded-xl font-bold text-sm' onClick={handleSendComment}>SEND</button>
+                <button className='bg-[#5357B6] ml-2 py-1 px-2 rounded-xl font-bold text-sm' onClick={handleSendComment}>ND</button>
             </div>
         </div>
     )
