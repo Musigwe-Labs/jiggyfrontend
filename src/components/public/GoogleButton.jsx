a00653a3332914ff14b2d92fdea07cdd3fea2bf7
churchill nnanna <mrxyfactor@gmail.com>
2023-08-24 11:49:19 +0100

    "added google and MSoft button to register page"
    Did some minor changes on tyhe google button too. changed it from a link
    tag to b button which emits event on click.

--- src/components/public/GoogleButton.jsx
+++ src/components/public/GoogleButton.jsx
@@ -2,18 +2,21 @@ import googleLogo from '/src/assets/google.png'
 import { GoogleOAuthProvider, useGoogleLogin} from '@react-oauth/google'
 
 
- export default function GoogleButton ({success, err}){
 
- 
+ export default function GoogleButton (){
+
+ 	const authUrl="https://accounts.google.com/o/oauth2/auth?client_id=524267745289-99tcul9q2eos9crnc5krameenh2p59gb.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fcruise.pythonanywhere.com%2Faccount%2Fauth%2Fgoogle%2Fcallback%2F&response_type=code&scope=email"
+
+ 	function googleAuth(){
+ 		window.location.replace(authUrl)
+ 	}
 
 
 	return(
-		<a href="https://accounts.google.com/o/oauth2/auth?client_id=524267745289-99tcul9q2eos9crnc5krameenh2p59gb.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fcruise.pythonanywhere.com%2Faccount%2Fauth%2Fgoogle%2Fcallback%2F&response_type=code&scope=email">
-		<button className="flex justify-center items-center  border py-2 px-6 gap-x-2 w-36 rounded-lg border-slate-500" >
+		<button className="flex justify-center items-center  border py-2 px-6 gap-x-2 w-36 rounded-lg border-slate-500" onClick={()=>googleAuth()} >
 			<img  src={googleLogo} alt="google logo"  />
 			<p>Google</p>
 		</button>
-		</a>
-		
+	
 		)
 }
