3facef6770803a8736b41cbfe4061a78b44db91a
Churchill Nnanna <68286428+chapter-codes@users.noreply.github.com>
2024-01-17 20:41:58 +0100

    Update AuthContext.jsx

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -27,12 +27,10 @@
      //   setKey(token);
      // }
           runAsync()
-
           async function runAsync(){
            //load user_details from backend
            try{
              const user=  await getUser({ queryKey: [null, key] })
-
            }catch (err){
              console.log(err)
              if(err?.response?.status==401){
@@ -45,30 +43,11 @@
              }
            }
           }
-   
-
-
-     // let fetchUser = async () => {
-     //   const headers = {
-     //   Authorization: `Token ${token}`,
-     // };
-     //   try{
-     //      const user_response = await axios.get("account/annonyuser/", {
-     //         headers,
-     //       });
-     //       setUserDetails(user_response.data)
-     //   }catch(err){
-     //     console.log(err)
-     //     setAppError(err)
-     //   }
-     // }
-     // if(!!token) fetchUser();
-
 
    }, [key]);
 
    return (
-     <AuthContext.Provider value={{ key, logout, userDetails, error, setError, redirect, setRedirect }}>
+     <AuthContext.Provider value={{ key, setKey, logout, userDetails, setUserDetails, error, setError, redirect, setRedirect }}>
        {props.children}
      </AuthContext.Provider>
    );
@@ -84,82 +63,3 @@
  }
 
  export {AuthContextProvider, useAuthContext , AuthContext};
-
-
-// /* eslint-disable react/prop-types */
-// import { createContext, useEffect, useState, useContext } from "react";
-// import axios from "../services/axios";
-// import { getUSerLoginToken } from "../utils/localStorage";
-// import { getUser } from "../utils/user";
-
-// import { useErrorContext } from "./ErrorContext";
-
-// const AuthContext = createContext();
-// const token= getUSerLoginToken()
-
-// const AuthContextProvider = (props) => {
-//   const [key, setKey] = useState(token);
-//   const [userDetails, setUserDetails] = useState({});
-//   const [error, setError] = useState("")
-//   const {setAppError} = useErrorContext()
-
-//   const logout = () => {
-//     setKey("");
-//     localStorage.removeItem("login");
-//     window.location.href = "/login";
-//   };
-
-//   useEffect(()=>{
-//     // if (token!=key) {
-//     //   setKey(token);
-//     // }
-//     // let fetchUser = async () => {
-//     //   const headers = {
-//     //   Authorization: `Token ${token}`,
-//     // };
-//     //   try{
-//     //      const user_response = await axios.get("account/annonyuser/", {
-//     //         headers,
-//     //       });
-//     //       setUserDetails(user_response.data)
-//     //   }catch(err){
-//     //     console.log(err)
-//     //     setAppError(err)
-//     //   }
-//     // }
-//     // if(!!token) fetchUser();
-
-//    try{
-//       const user=  await getUser({ queryKey: [null, key] })
-
-//     }catch (err){
-//       console.log(err)
-//       // if(err?.response?.status==401){
-//       //   console.log(true)
-//       //   removeFieldFromLS('login') // remove lodin from localStorag
-//       //   setKey(null)
-//       //   setRedirect({url:'/login'})
-//       // }else{
-//       //   setAppError(err)
-//       // }
-//     }
-
-//   })(), [key]);
-
-//   return (
-//     <AuthContext.Provider value={{ key, logout, userDetails, error, setError }}>
-//       {props.children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// const useAuthContext=()=>{
-//   const context= useContext(AuthContext)
-//   if(!context){
-//     return console.error('Authcontext must be used within Authcontext Provider')
-//   }
-
-//   return context
-// }
-
-// export {AuthContextProvider, useAuthContext , AuthContext};
